2a8fd8897a7cc3309a76794f92a7c8f5dc99ded0
周家合 <8519554+zhou-jiahe@user.noreply.gitee.com>
2021-06-10 21:10:43 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.axios.get("/js/seller.json").then((res) => {
+      console.log(res.data);
+    });
+  },
+};
+</script>
